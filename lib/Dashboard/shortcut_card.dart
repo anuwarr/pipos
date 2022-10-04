294cde23af0123e698c20421a8e71bec9ae784ac
Ahmed Anwar <anwar@bml.com.mv>
2022-10-04 11:59:44 +0500

    shortcut

--- lib/Dashboard/shortcut_card.dart
+++ lib/Dashboard/shortcut_card.dart
@@ -1,11 +1,10 @@
-
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class ShortCutCards extends StatefulWidget {
-  final  Color color;
+  final Color color;
   final String statName;
   final String totalStat;
   const ShortCutCards({
@@ -24,31 +23,51 @@ class _StatCardState extends State<ShortCutCards> {
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
-      onEnter: (value){
+      onEnter: (value) {
         setState(() {
           hovered = true;
         });
       },
-      onExit: (value){
-
+      onExit: (value) {
         setState(() {
           hovered = false;
         });
       },
       child: AnimatedContainer(
-        duration: const Duration(milliseconds: 275),
-        height: hovered ? 160.0 : 155.0 ,
+        duration: const Duration(milliseconds: 300),
+        height: hovered ? 160.0 : 155.0,
         width: hovered ? 200.0 : 195.0,
         decoration: BoxDecoration(
-          color: hovered ? widget.color : Colors.white,
-          borderRadius: BorderRadius.circular(15.0),
-          border: Border.all(color: Colors.black)
-          
-          
-          
-
-        ),
-        ),
+            color: hovered ? widget.color : Colors.white,
+            borderRadius: BorderRadius.circular(15.0),
+            border: Border.all(color: Colors.black)),
+        child: Center(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 13.0,
+                ),
+                Container(
+                  height: 30.0,
+                  width: 30.0,
+                  child: Icon(
+                    Icons.shopping_cart,
+                    color: !hovered ? Colors.grey : Colors.black,
+                    size: 25,
+                  ),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(30.0)),
+                )
+              ],
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
